6986c925e831cf5d374beaf19bd573a66d9e00ec
sajidpanorama <sajid@panoramamas.com>
2025-08-07 19:41:37 +0600

    role updated

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -214,7 +214,7 @@ export default function HeroSection() {
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.7, delay: 0.65 }}
           >
-            Front-End Web Developer
+            Software Developer
           </motion.h2>
 
           {/* Bio */}
